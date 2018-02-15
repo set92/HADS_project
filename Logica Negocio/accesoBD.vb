821bb02c03b54c1c681a895775341ea25c86ecc1
Imanol Atienza <imanolatienza@gmail.com>
2018-02-15 13:54:39 +0100

    Version completa sin probar fallos

--- Logica Negocio/accesoBD.vb
+++ Logica Negocio/accesoBD.vb
@@ -26,7 +26,7 @@ Public Class accesoBD
     End Function
 
     Public Shared Function insertarUsuario(ByVal datos As Array) As String
-        Dim sql = "insert into Usuarios(email, nombre, apellidos, numconfir, confirmado, tipo, pass) values('" & datos(0) & "', '" & datos(1) & "', '" & datos(2) & "', " & datos(3) & ", " & datos(4) & ", '" & datos(5) & "')"
+        Dim sql = "insert into Usuarios(email, nombre, apellidos, numconfir, confirmado, tipo, pass) values('" & datos(0) & "', '" & datos(1) & "', '" & datos(2) & "', " & datos(3) & ", " & datos(4) & ", '" & datos(5) & "', " & datos(6) & ")"
         Dim numRegistro As Integer
 
         Try
@@ -50,7 +50,52 @@ Public Class accesoBD
                 Return "NO EXISTE EL USUARIO"
             End If
         Catch ex As Exception
-            Return "ERROR AL CONSULTAR EL USUARIO: " + ex.Message
+            Return "ERROR AL IDENTIFICAR EL USUARIO: " + ex.Message
+        End Try
+    End Function
+
+    Public Shared Function existeMail(ByVal mail As String) As String
+        Dim sql = "select count(email) from Usuarios where email='" & mail & "'"
+
+        Try
+            comando = New SqlCommand(sql, conexion)
+            If comando.ExecuteScalar() = 1 Then
+                Return "EXISTE MAIL OK"
+            Else
+                Return "NO EXISTE EL MAIL"
+            End If
+        Catch ex As Exception
+            Return "ERROR AL COMPROBAR EL MAIL: " + ex.Message
+        End Try
+    End Function
+
+    Public Shared Function cambiarPassword(ByRef datos As Array) As String
+        Dim sql = "update Usuarios set pass='" & datos(1) & "' where email='" & datos(0) & "'"
+
+        Try
+            comando = New SqlCommand(sql, conexion)
+            If comando.ExecuteNonQuery() = 1 Then
+                Return "CONTRASEÑA CAMBIADA OK"
+            Else
+                Return "NO EXISTE UN USUARIO CON ESA CONTRASEÑA"
+            End If
+        Catch ex As Exception
+            Return "ERROR AL CAMBIAR LA CONTRASEÑA DEL USUARIO: " + ex.Message
+        End Try
+    End Function
+
+    Public Shared Function loginUsuario(ByRef datos As Array) As String
+        Dim sql = "select count(email) from Usuarios where email='" & datos(0) & "' and pass='" & datos(1) & "'"
+
+        Try
+            comando = New SqlCommand(sql, conexion)
+            If comando.ExecuteScalar() = 1 Then
+                Return "LOGIN USUARIO OK"
+            Else
+                Return "NO EXISTE ESE USUARIO"
+            End If
+        Catch ex As Exception
+            Return "ERROR AL ACCEDER EL USUARIO: " + ex.Message
         End Try
     End Function
 End Class
